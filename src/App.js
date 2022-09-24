c07145a6b2f8d7b8868a9f5f364e6cca9d9e004e
Kiran Kamble <kirankamble4595@gmail.com>
2022-09-24 23:11:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import {useEffect, useMemo, useState} from "react"
 function App() {
+  const [text, setText] = useState('')
+  const [age, setAge] = useState(null)
+  const [country, seCountry] = useState('')
+
+  const userType = useMemo(() =>(
+    {
+      underAge : age <18 ? true : false,
+      citizen : country === "USA" ? true : false,
+    }
+  ))
+
+  useEffect(() =>{
+    console.log("User type has changed..");
+  },[userType])
+  console.log("Component re-rendered");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h2>useMemo Example</h2>
+      <input placeholder='name' onChange={(e) => setText(e.target.value)}></input>
+      <br></br>
+      <input placeholder='age' type="number" onChange={(e) => setAge(e.target.value)}></input>
+      <br></br>
+      <select>
+        <option value="USA">USA</option>
+        <option value="UK">UK</option>
+        <option value="INDIA">INDIA</option>
+        <option value="MALESIA">MALESIA</option>
+        <option value="DUBAI">DUBAI</option>
+      </select>
+      
+     
     </div>
   );
 }
